0496b6a85e83c9e9f90242bd0228f51858fa0271
Tjcarroll1 <tjcarroll1@icloud.com>
2024-10-20 20:19:19 +1000

    added login path

--- api/sanity.config.ts
+++ api/sanity.config.ts
@@ -1,17 +1,13 @@
-import {defineConfig} from 'sanity'
-import {structureTool} from 'sanity/structure'
-import {schemaTypes} from './schemaTypes'
+import { defineConfig } from 'sanity';
+import { schemaTypes } from './schemaTypes';
 
 export default defineConfig({
   name: 'default',
   title: 'SanityBoilerPlate',
-
   projectId: '2hcufetx',
   dataset: 'production',
-
-  plugins: [structureTool() ],
-
+  basePath: '/admin', // This sets the Studio to be served at /admin
   schema: {
     types: schemaTypes,
   },
-})
+});
